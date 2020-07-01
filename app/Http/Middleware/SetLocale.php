eae30ddea372094f1bde6358a7fcf8c23c0576ca
Gertjan Roke <g.a.roke90@gmail.com>
2020-07-01 19:40:56 +0200

    Fixed locale for the validation example thanks to the latest MR about it
    Signed-off-by: Gertjan Roke <g.a.roke90@gmail.com>

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -4,7 +4,6 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Session;
 
 class SetLocale
 {
@@ -18,20 +17,8 @@ class SetLocale
      */
     public function handle($request, Closure $next)
     {
-        $locale = $request->route('locale') ?: Session::get('locale');
-
-        if ($locale) {
-            App::setLocale($locale);
-
-            /**
-             * We need to store the locale in the session because we don't want to pass it to each component we create.
-             * Also because we fetch the locale from the route but the url for the Livewire API endpoint only
-             * contains the component name.
-             */
-            Session::put('locale', $locale);
-        } else {
-            // Forget the locale so other examples will not be conflicted by it.
-            Session::forget('locale');
+        if ($request->route('locale')) {
+            App::setLocale($request->route('locale'));
         }
 
         return $next($request);
